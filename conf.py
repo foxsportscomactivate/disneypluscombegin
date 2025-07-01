c95a6be3a183d06d4da9eef3d31e8b5b99c765f5
foxsportscomactivate <radhjain1@gmail.com>
2025-07-01 12:06:28 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Begintv'
 
 
 # The full version, including alpha/beta/rc tags
